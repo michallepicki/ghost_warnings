ff51614fbcc40eea35ac6b2e0bfdad49e36c02b1
Michał Łępicki <michallepicki@users.noreply.github.com>
2024-12-20 15:36:04 +0100

    uncomment function component

--- lib/thingy.ex
+++ lib/thingy.ex
@@ -3,9 +3,9 @@ defmodule Thingy do
 
   embed_templates "templates/*"
 
-  #def function_component(assigns) do
-  #  ~H"""
-  #  <div data-warning-shown-every-time={GhostWarnings.color(@user) == :red}></div>
-  #  """
-  #end
+  def function_component(assigns) do
+    ~H"""
+    <div data-warning-shown-every-time={GhostWarnings.color(@user) == :red}></div>
+    """
+  end
 end
